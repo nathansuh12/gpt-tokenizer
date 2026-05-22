e61d3495c3f330f45748ab02c4eba8ad4ec81512
nathan suh <natesuh1@gmail.com>
2026-05-22 21:11:46 +0200

    test cases from Andrej Karpathy "minbpe"

---
+++ test_tokenizer.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+
+from exercise import BasicTokenizer
+
+test_strings = [
+    "", # empty string
+    "?", # single character
+    "hello world!!!? (안녕하세요!) lol123 😉", # fun small string
+    "FILE:taylorswift.txt", # FILE: is handled as a special string in unpack()
+]
+def unpack(text):
+    # we do this because `pytest -v .` prints the arguments to console, and we don't
+    # want to print the entire contents of the file, it creates a mess. So here we go.
+    if text.startswith("FILE:"):
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        taylorswift_file = os.path.join(dirname, text[5:])
+        contents = open(taylorswift_file, "r", encoding="utf-8").read()
+        return contents
+    else:
+        return text
+    
+@pytest.mark.parametrize("tokenizer_factory", [BasicTokenizer])
+@pytest.mark.parametrize("text", test_strings)
+def test_encode_decode_identity(tokenizer_factory, text):
+    text = unpack(text)
+    tokenizer = tokenizer_factory()
+    ids = tokenizer.encode(text)
+    decoded = tokenizer.decode(ids)
+    assert text == decoded
+
+if __name__ == "__main__":
+    pytest.main()
